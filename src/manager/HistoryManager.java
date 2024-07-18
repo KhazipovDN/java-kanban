68b2750317c0a4efe7027a973702f9e355a04aee
Dinar Khazipov <khazipovdn@yandex.ru>
2024-07-18 23:24:00 +0300

    ver12

--- src/manager/HistoryManager.java
+++ src/manager/HistoryManager.java
@@ -5,7 +5,10 @@ import model.AbstractTask;
 import java.util.List;
 
 public interface HistoryManager {
+
     void remove(int id);
+
     void add(AbstractTask abstractTask, int id);
+
     List<AbstractTask> getHistory();
 }
