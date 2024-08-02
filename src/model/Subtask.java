ed8fe548fb580f614a11d37646fb0e450d0bd662
Dinar Khazipov <khazipovdn@yandex.ru>
2024-08-03 00:26:30 +0300

    ТЗ 7 ver5

--- src/model/Subtask.java
+++ src/model/Subtask.java
@@ -24,7 +24,7 @@ public class Subtask extends AbstractTask {
 
     @Override
     public String toString() {
-        return id+
+        return id +
                 ",SUBTASK" +
                 "," + name +
                 "," + status +
